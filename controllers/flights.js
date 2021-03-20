a401f646713d6645a968eb9af711eec803fd7262
github_username <lordsnoosh@gmail.com>
2021-03-19 17:25:35 -0700

    controller framework insp.Shaw

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,27 +1,68 @@
 const Flight = require('../models/flight');
 
 module.exports = {
-    index,
-    new: newFlight,
-    create
-  };
+  index,
+  show,
+  create,
+  update,
+  delete: deleteFlight
+};
+function index(req, res) {
+  Flight.find({}, function(err, flights) {
+      if (err) console.log(err);
+      res.send(flights);
+  });
+}
+function show(req, res) {
+  Flight.findById(req.params.id, function(err, flights) {
+      if (err) console.log(err);
+      res.send(flight);
+  });
+}
+function create(req, res) {
+  Flight.create(req.body, function(err, flight) {
+      if (err) console.log(err);
+      res.redirect('/flights/$flight._id}');
+  })
+}
+function update(req, res) {
+  Flight.findById(req.params.id, function(err, flights) {
+      if (err) console.log(err);
+      const updatedFlight = Object.assign(flight, req.body);
+      Flight.findByIdAndUpdate(req.params.id, updatedFlight, function(err, flght) {
+          res.redirect('/flight/${req.params.id}');
+      });
+  });
+}
+function deleteFlight(req, res) {
+  Flight.findByIdAndDelete(req.params.id, function(err, deletedFlight) {
+      if (err) console.log(err);
+      res.redirect('/flights');
+  })
+}
+
+// module.exports = {
+//     index,
+//     new: newFlight,
+//     create
+//   };
   
-  function index(req, res) {
-    Flight.find({}, function(err, flights) {
-      res.render('flights/index', { flights });
-    });
-  }
+//   function index(req, res) {
+//     Flight.find({}, function(err, flights) {
+//       res.render('flights/index', { flights });
+//     });
+//   }
   
-  function create(req, res) {
+//   function create(req, res) {
    
-    const flight = new Flight(req.body);
-    flight.save(function(err) {
-      if (err) return res.render('flights/new');
-      console.log(flight);
-      res.redirect('/flights/new');
-    });
-  }
+//     const flight = new Flight(req.body);
+//     flight.save(function(err) {
+//       if (err) return res.render('flights/new');
+//       console.log(flight);
+//       res.redirect('/flights/new');
+//     });
+//   }
   
-  function newFlight(req, res) {
-    res.render('flights/new');
-  } 
+//   function newFlight(req, res) {
+//     res.render('flights/new');
+//   } 
