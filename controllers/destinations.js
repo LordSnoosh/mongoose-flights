220da0a4f037726f962209de41bc8333bb2b75c3
github_username <lordsnoosh@gmail.com>
2021-03-22 14:50:29 -0700

    destination adding

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -1,15 +1,13 @@
 const Flight = require('../models/flight');
-
 module.exports = {
     create
 };
-
 function create(req, res) {
-    Flight.findById(req.params.id, function(err, destinations) {
-        flight.arrivals.push(req.body);
+    Flight.findById(req.params.id, function(err, flight) {
+        //We can push subdocs into mongoose arrays
+        flight.destinations.push(req.body);
         flight.save(function(err) {
-            console.log(flight, flight.arrivals[0]);
-          res.redirect(`/flights/${flight._id}`);
+            res.redirect(`/flights/${flight._id}`);
         });
-      });
-};
+    });
+}
